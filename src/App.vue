479b6dc76e3cdcbc5a3e7da167b2e09e439818bd
WzrywProstaty <joint353@gmail.com>
2023-10-26 14:56:41 +0300

    Removed Todo.vue; transfered it's code into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
-<script setup lang="ts">
-import './assets/styles/global.scss'
-
-import Todo from './components/Todo.vue';
-</script>
-
 <template>
-  <Todo></Todo>
+  <header>
+    <Todo_TaskFilterBar/>
+    <Todo_TaskList :route="route"/>
+  </header>
 </template>
+
+<script lang="ts">
+import Todo_TaskFilterBar from './components/Todo_TaskFilterBar.vue'
+import Todo_TaskList from "./components/TaskList/Todo_TaskList.vue";
+import {useRoute} from "vue-router";
+export default {
+  setup(){
+    const route = useRoute();
+    return{
+      route
+    }
+  },
+  components:{
+    Todo_TaskFilterBar,
+    Todo_TaskList
+  }
+}
+</script>
 
 <style scoped>
 
