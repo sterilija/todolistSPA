636762b910a3e10c73fb93d765bf3fe36ceeb183
WzrywProstaty <joint353@gmail.com>
2023-10-28 14:06:25 +0300

    The tasks array now been brought into App.vue and transfered back as a prop

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <header>
     <Todo_TaskFilterBar/>
-    <Todo_TaskList :route="route"/>
-    <Todo_TaskAdd></Todo_TaskAdd>
+    <Todo_TaskList :route="route" :tasksArr="tasksArr"/>
+    <Todo_TaskAdd :tasksArr="tasksArr"></Todo_TaskAdd>
   </header>
 </template>
 
@@ -23,6 +23,40 @@ export default {
     Todo_TaskList,
     Todo_TaskAdd,
   },
+  data:()=>({
+    tasksArr: [
+      {
+        id: 0,
+        shortDesc: 'anyone who loves or pursues',
+        text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget Curabitur ullamcorper ultricies nisi. ipsum dolor sit amet, cAenean commodo ligula eget Curabitur ullamcorper',
+        finished: false
+      },
+      {
+        id: 1,
+        shortDesc: 'rugs and riches',
+        text: 'ligula eget Curabitur ullamcorper adipiscing elit. Aenean ultricies nisi.',
+        finished: true
+      },
+      {
+        id: 2,
+        shortDesc: 'wash your pants',
+        text: 'ligula eget Curabitur ullamcorper adipiscing elit. Aenean ultricies nisi.',
+        finished: true
+      },
+      {
+        id: 3,
+        shortDesc: 'rice and boiled water',
+        text: 'ligula eget Curabiturit. Aenean ult ulit. Aenean ultricies ullamcorper nisi.',
+        finished: false
+      },
+      {
+        id: 4,
+        shortDesc: 'cats dogs and puss',
+        text: 'ligula eget Curabitur ullamcorper adipiscing elit. Aenean ultricies nisi.',
+        finished: false
+      }
+    ],
+  }),
 }
 </script>
 
